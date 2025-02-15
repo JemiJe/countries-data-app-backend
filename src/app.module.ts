4535ddf89b700bb7d16d0b4cb7352b628bb80d26
Jeremy <>
2025-02-15 14:14:28 +0200

    feat: added config for envs and .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { CountryModule } from './country/country.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    HttpModule,
+    // CountryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
